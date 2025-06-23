38f251c2a51c27f2ec96224d39414afe5ca3b8ef
StasiyaN <ana.nyzhalska@gmail.com>
2025-06-23 18:28:49 +0200

    homework done

--- homework.js
+++ homework.js
@@ -1,7 +1,7 @@
 //1
-function isPalindrome(string) {
-    const reversedString = string.split('').reverse().join('');
-    return string === reversedString
+function isPalindrome(word) {
+    const reversedWord = word.split('').reverse().join('');
+    return word === reversedWord
 }
 
 console.log(isPalindrome('казак'))
@@ -10,3 +10,68 @@ console.log(isPalindrome('анна'))
 console.log(isPalindrome('аня'))
 
 //2
+function shortestWord (phrase) {
+    const words = phrase.split(/\s+/);;
+    let shortestWord = words[0];
+    for (let word of words) {
+        if (word.length < shortestWord.length ) {
+            shortestWord = word;
+        }
+    }
+    console.log(shortestWord)
+}
+
+shortestWord('My cat is cute')
+shortestWord('Banana is yellow')
+
+//3
+function createPhoneNumber(input) {
+    // Преобразуем вход в строку и удалим всё, кроме цифр
+    const digits = String(input).replace(/\D/g, '');
+
+    const part1 = digits.slice(0, 3); // код
+    const part2 = digits.slice(3, 6); // первые 3 цифры
+    const part3 = digits.slice(6);    // последние 3 цифры
+
+    return `8 (${part1}) ${part2}-${part3}`;
+}
+
+//4
+function findMinMaxInArray(arr) {
+    const min = Math.min(...arr);
+    const max = Math.max(...arr);
+
+    return {min, max};
+}
+
+const arrayOfNumbers = [1, 10, 80, 68, 12, 0.5]
+console.log(findMinMaxInArray(arrayOfNumbers))
+
+//5
+function bubbleSort(arr) {
+    // Копируем массив, чтобы не менять оригинальный
+    let result = arr.slice();
+
+    let n = result.length;
+    let swapped;
+
+    do {
+        swapped = false;
+        for (let i = 0; i < n - 1; i++) {
+            if (result[i] > result[i + 1]) {
+                // Меняем местами
+                let temp = result[i];
+                result[i] = result[i + 1];
+                result[i + 1] = temp;
+
+                swapped = true;
+            }
+        }
+        n--; // После каждой итерации можно не проверять последний элемент
+    } while (swapped);
+
+    return result;
+}
+
+const numbersToSort = [ 1, 6, 10, 15, 18, 100]
+console.log(bubbleSort(numbersToSort))
